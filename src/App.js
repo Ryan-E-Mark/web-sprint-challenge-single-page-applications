b735b4ff48d97eafc0cba6af1d5b19f3496c1439
Ryan Mark <rymark907@gmail.com>
2021-09-17 10:33:54 -0700

    Setting up form and useState

--- src/App.js
+++ src/App.js
@@ -23,11 +23,41 @@ const App = () => {
   const initialFormErrors = {
     name: '',
     size: '',
-    topping1: false,
-    topping2: false,
-    topping3: false,
-    topping4: false,
-    topping5: false,
+    sauce: '',
+  }
+
+  const initialPizzas = [];
+  const initialDisabled = true;
+
+  const [pizzas, setPizzas] = useState(initialPizzas);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  const onChange = (name, value) => {
+    setFormValues({...formValues, [name]: value});
+  }
+
+  const postPizza = newPizza => {
+    axios.post("https://reqres.in/api/orders", newPizza)
+      .then(resp => {
+        console.log(resp)
+      }).catch(err => console.log(err))
+  }
+
+  const onSubmit = () => {
+    const newPizza = {
+      name: formValues.name.trim(),
+      size: formValues.size.trim(),
+      sauce: formValues.sauce,
+      topping1: formValues.topping1,
+      topping2: formValues.topping2,
+      topping3: formValues.topping3,
+      topping4: formValues.topping4,
+      topping5: formValues.topping5,
+      special: formValues.special,
+    }
+    postPizza(newPizza);
   }
 
 
@@ -38,7 +68,12 @@ const App = () => {
           <Home />
         </Route>
         <Route path={"/pizza"}>
-          <PizzaForm />
+          <PizzaForm 
+            values={formValues}
+            onSubmit={onSubmit}
+            onChange={onChange}
+            disabled={disabled}
+            />
         </Route>
       </Switch>
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,21 @@ import { Link } from 'react-router-dom';
 import "../Form.css";
 
 export default function PizzaForm(props) {
+
+    const { onChange, values, onSubmit, disabled } = props;
+
+    const submit = event => {
+        event.preventDefault();
+        onSubmit();
+    }
+
+    const change = event => {
+        const { name, value, checked, type } = event.target;
+        const valueUsed = type === 'checkbox' ? checked : value;
+        onChange(name, valueUsed);
+    }
+
+
     return(
         <div>
             <nav>
@@ -15,15 +30,38 @@ export default function PizzaForm(props) {
             </nav>
             <section className="form-page">
                 <div className="form-div">
-                    <form id="pizza-form">
+                    <form id="pizza-form" onSubmit={submit}>
                         <label>
                             Name for the order: 
                             <input 
                                 id="name-input"
                                 type="text"
                                 name="name"
+                                value={values.name}
+                                onChange={change}
                                 />
                         </label>
+                        <label>
+                            Select your size:
+                            <select onChange={change} value={values.size} name="size" id="size-dropdown">
+                                <option value="">
+                                    --Select a Size--
+                                </option>
+                                <option value="small">
+                                    Small
+                                </option>
+                                <option value="medium">
+                                    Medium
+                                </option>
+                                <option value="large">
+                                    Large
+                                </option>
+                                <option value="extralarge">
+                                    Extra Large
+                                </option>
+                            </select>
+                        </label>
+                        <button disabled={disabled}>Submit your order!</button>
                     </form>
                 </div>
             </section>
